7531e0e0ea3ee7015a2907fdeab9c595eb74d099
Kevin Nash <kjn33@case.edu>
2017-02-26 18:14:05 -0500

    Moved flags into struct. Enforced mutual exclusion of some options. Made names more conventional.

--- Project 2/PacketTrace.c
+++ Project 2/PacketTrace.c
@@ -21,52 +21,63 @@ struct metadata {
     uint16_t caplen;
 };
 
+static struct {
+    /* specifies printing a trace summary */
+    int s;
+    /* specifies printing ethernet headers */
+    int e;
+    /* specifies printing ip headers */
+    int i;
+    /* specifies printing a count of packet types */
+    int t;
+    /* specifies printing a traffic matrix */
+    int m;
+} options;
+
+void write_output(FILE *trace_fileptr);
+
 int main(int argc, char **argv) {
     
     /* reusable counting variable */
     int i;
     /* iterator for command line args */
     int option;
-
-    /* specifies printing a trace summary */
-    int flag_s = 0;
-    /* specifies printing ethernet headers */
-    int flag_e = 0;
-    /* specifies printing ip headers */
-    int flag_i = 0;
-    /* specifies printing a count of packet types */
-    int flag_t = 0;
-    /* specifies printing a traffic matrix */
-    int flag_m = 0;
+    /* count of the optional flags provided */
+    int option_cnt;
 
     /* filename for the binary packet trace file */
-    char *traceFilename = NULL;
-    /* pointer to the file opened by traceFilename */
-    FILE *traceFilePtr = NULL;
+    char *trace_filename = NULL;
+    /* pointer to the file opened by trace_filename */
+    FILE *trace_fileptr = NULL;
 
     /* external variable set to zero to override getopt error handling */
     opterr = 0;
 
     // Parse arguments
-    while ((option = getopt(argc, argv, "r:seit")) != -1)
+    while (-1 != (option = getopt(argc, argv, "r:seitm")))
     switch (option) {
         case 's':
-            flag_s = 1;
+            options.s = 1;
+            option_cnt++;
             break;
         case 'e':
-            flag_e = 1;
+            options.e = 1;
+            option_cnt++;
             break;
         case 'i':
-            flag_i = 1;
+            options.i = 1;
+            option_cnt++;
             break;
         case 't':
-            flag_t = 1;
+            options.t = 1;
+            option_cnt++;
             break;
         case 'm':
-            flag_m = 1;
+            options.m = 1;
+            option_cnt++;
             break;
         case 'r':
-            traceFilename = optarg;
+            trace_filename = optarg;
             break;
         case '?':
             if (optopt == 'r') {
@@ -85,28 +96,33 @@ int main(int argc, char **argv) {
     // Tell the user they typed something wrong
     for (i = optind; i < argc; i++) {
         fprintf(stderr, "Invalid argument: %s\n", argv[optind]);
-        exit(EXIT_FAILURE); // Can be removed if we don't want to fail-fast
+        exit(EXIT_FAILURE); // Can be removed if we want to ignore unknown args
+    }
+
+    // Enforce mutual exclusion of options
+    if (1 < option_cnt) {
+        fprintf(stderr, "Specified multiple mutually exclusive options.\n");
+        exit(EXIT_FAILURE);
     }
 
     // Check whether an input filename was given
-    if (traceFilename == NULL) {
+    if (trace_filename == NULL) {
         printf("Use of -r <filename> is required.\n");
         exit(EXIT_FAILURE);
     } else {
         // Attempt to open the specified file
-        traceFilePtr = fopen(traceFilename, "rb");
-        if (traceFilePtr == NULL) {
+        trace_fileptr = fopen(trace_filename, "rb");
+        if (trace_fileptr == NULL) {
             perror("Error");
             exit(EXIT_FAILURE);
         }
     }
 
-    if (flag_s) {};
-    if (flag_e) {};
-    if (flag_i) {};
-    if (flag_t) {};
-    if (flag_m) {};
-    
+    write_output(trace_fileptr);
     exit(EXIT_SUCCESS);
+}
+
+void write_output(FILE *trace_fileptr) {
+    printf("s option: %d\n", options.s);
 }
 
